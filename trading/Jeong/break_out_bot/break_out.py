0b7d5dc12245f6fdcd21b5c7cb31eef1f28b9e96
Jin <thinkjin99@gmail.com>
2022-02-17 14:48:45 +0900

    uuid error

--- trading/Jeong/break_out_bot/break_out.py
+++ trading/Jeong/break_out_bot/break_out.py
@@ -1,4 +1,6 @@
 from datetime import date, datetime
+
+from matplotlib import ticker
 import upbit
 import time
 class BreakOut():
@@ -12,32 +14,64 @@ class BreakOut():
         target = today['opening_price'] + (high - low) * 0.5
         return target
     
-    def check_mid_night(self):
+    def check_reset_time(self):
         now = datetime.now()
-        return (now.hour == 0) and (now.minute == 0) and (now.second == 0) 
+        return (now.hour == 9) and (now.minute == 0) and (now.second == 0)
+
+    def create_buy_order(self, ticker, now_price, target_price):
+        if now_price >= target_price:
+            krw = float((self.upbit.get_my_account()[0])['balance'])
+            orderbook = (self.upbit.get_order_book(ticker)[0])['orderbook_units']
+            sell_price = float(orderbook[0]['ask_price']) #호가창에서 가장 저렴한 매도 가격
+            unit = round((krw / sell_price),5)
+        if krw >= 5000: #최소 무준 금액
+            print(f'Make order at {now_price}, Unit: {unit}, Price: {sell_price}, Total Buy:{sell_price * unit} ')
+            res = self.upbit.create_order(ticker, 'bid', unit, sell_price,"limit")
+        return res
     
+    def create_sell_order(self,ticker):
+        orderbook = (self.upbit.get_order_book(ticker)[0])['orderbook_units']
+        buy_price = float(orderbook[-1]['bid_price'])
+        asset = self.upbit.get_my_account()
+        for i,a in enumerate(asset):
+            if a['currency'] == 'ticker':
+                index = i
+                break
+        try:
+            unit = asset[index]['balance']
+        except Exception as e:
+            print(e)
+            print("No such ticker in asset")
+            return
+
+        res = self.upbit.create_order(ticker,'ask', unit, buy_price,"limit")
+        return res
+
+
+
+
     def run(self):
         target_price = self.get_target_price()
+        target_price = 1000
+        sleep_sec = 60
+        ticker = "KRW-BTC"
         while True:
             try:
-                if self.check_mid_night():
+                if self.check_reset_time():
                     target_price = self.get_target_price()
-                now_price = self.upbit.get_current_price('KRW-BTC')[0]['trade_price']
+                    self.create_sell_order(ticker)
+                now_price = self.upbit.get_current_price(ticker)[0]['trade_price']
                 if now_price >= target_price:
-                    krw = (self.upbit.get_my_account()[0])['balance']
-                    orderbook = self.upbit.get_order_book()['orderbook_units']
-                    sell_price = orderbook[0]['ask_price'] #호가창에서 가장 저렴한 매도 가격
-                    unit = krw / sell_price
-                    if krw > 5000:
-                        self.upbit.make_order('KRW-BTC', unit, sell_price)
-                self.make_log(now_price, target_price)
+                    self.create_buy_order(ticker, now_price, target_price)
+                self.make_log(ticker,now_price, target_price)
+                sleep_sec = 1 if abs(now_price / target_price) >= 0.97 else 1
             except Exception as e:
                 print(e)
-            time.sleep(1)
+            time.sleep(sleep_sec)
 
-    def make_log(self,now_price,target_price):
+    def make_log(self,ticker,now_price,target_price):
         now = datetime.now()
-        print(f"{now} , target: {target_price}, now: {now_price}")
+        print(f"{now} , ticker: {ticker} target: {target_price}, now: {now_price}")
 
 
 bot = BreakOut()
--- trading/Jeong/break_out_bot/upbit.py
+++ trading/Jeong/break_out_bot/upbit.py
@@ -1,50 +1,62 @@
-from os import access
+from urllib import response
 import jwt
 import uuid
 import requests
 import author
 import hashlib
 from urllib.parse import urlencode
+import time
 class Upbit():
     def __init__(self):
         self.access_key = author.access_key
         self.secret_key = author.secret_key
         self.server_url = 'https://api.upbit.com/v1/'
-        self.payload = {'access_key': self.access_key,
-                    'nonce': str(uuid.uuid4())}
+        # self.payload = {'access_key': self.access_key,
+        #             'nonce': str(uuid.uuid4())}
         self.non_auth_headers = {"Accept": "application/json"}
 
-    def get_auth(self, payload):
+    def get_headers(self, payload):
         jwt_token = jwt.encode(payload, self.secret_key)
         authorize_token = 'Bearer {}'.format(jwt_token)
         headers = {"Authorization": authorize_token}
         return headers
 
+    def create_request(self,method,url,headers,params = None):
+        res = requests.request(method, url, params = params,headers=headers)
+        # while (res.status_code != 200 and res.status_code != 201):
+        #     res = requests.request(method, url, params = params, headers=headers)
+        #     print(f"Retry to get api: {res.json()}")
+        #     time.sleep(0.1)  
+        json = res.json()
+        return json
+
     def get_my_account(self):
-        headers = self.get_auth(self.payload)
-        my_asset = requests.get(self.server_url + "accounts",headers=headers).json()
-        return my_asset
+        url = f"{self.server_url}accounts"
+        response = self.create_request("GET", url, headers=self.get_headers(self.create_payload()))
+        return response
 
     def get_current_price(self,ticker):
         url = f"{self.server_url}ticker?markets={ticker}"
-        response = requests.get(url, headers=self.non_auth_headers)
-        return response.json()
+        response = self.create_request("GET", url, headers=self.non_auth_headers)
+        return response
     
     def get_order_book(self, ticker):
         url = f"{self.server_url}orderbook?markets={ticker}"
-        response = requests.request("GET", url, headers=self.non_auth_headers)
-        return response.json()
-
-    def create_payload(self, query_hash):
-        payload = self.payload.copy()
-        payload['query_hash'] = query_hash #payload에 쿼리 대입
-        payload['query_hash_alg'] = 'SHA512'
-        return payload
-
-    def get_day_candle(self, ticker, candle_type, count): #candle_type -> min,day, period -> 1min, 3min, 1day..
-        url = f"{self.server_url}candles/{candle_type}/?market={ticker}&count={count}"
-        response = requests.get(url, headers=self.non_auth_headers).json()
+        response = self.create_request("GET", url, headers=self.non_auth_headers)
         return response
+
+    def get_day_candle(self, ticker, candle_type="days", count = 1): #candle_type -> min,day, period -> 1min, 3min, 1day..
+        url = f"{self.server_url}candles/{candle_type}/?market={ticker}&count={count}"
+        response = self.create_request("GET",url, headers=self.non_auth_headers)
+        return response
+
+    def create_payload(self, query_hash = None):
+        payload = {'access_key': self.access_key,
+                    'nonce': str(uuid.uuid4())}
+        if query_hash:
+            payload['query_hash'] = query_hash #payload에 쿼리 대입
+            payload['query_hash_alg'] = "SHA512"
+        return payload
 
     def get_hash_url(self, query):
         query_string = urlencode(query).encode() #url 인코딩
@@ -53,18 +65,25 @@ class Upbit():
         query_hash = m.hexdigest() #쿼리 해싱 값 대입
         return query_hash
 
-    def make_order(self, ticker, volume, price,ord_type):
+    def create_order(self, ticker, side, volume, price, ord_type):
         query = {
             'market':ticker,
-            'side':'bid',
+            'side': side, #매수,매도
             'volume': volume,
             'price':price,
-            'ord_type': ord_type,
+            'ord_type': ord_type, #시장가, 지정가
         }
         query_hash = self.get_hash_url(query)
         payload = self.create_payload(query_hash)
-        headers = self.get_auth(payload)#헤더 반환
-        res = requests.post(self.server_url + "orders",params=query,headers=headers).json()
+        headers = self.get_headers(payload)#헤더 반환
+        url = f"{self.server_url}orders"
+        res = self.create_request("POST",url,headers,query)
         return res
     
+b = Upbit()
+# print(b.get_order_book("KRW-BTC"))
+# print(b.get_current_price("KRW-BTC"))
+# print(b.get_day_candle("KRW-BTC"))
+# print(b.get_my_account())
+print(b.create_order('KRW-BTC','bid',100,10000,"limit"))
 
